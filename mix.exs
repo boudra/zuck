dbace02d76fd29850465334d354491443bf155e7
Mohamed Boudra <boudra.moha@gmail.com>
2017-04-26 21:25:08 +0200

    package definition

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Zuck.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: "Tiny library for the Facebook Graph API",
+     package: [
+       maintainers: ["Mohamed Boudra"],
+       licenses: ["MIT"],
+       links: %{ "Github" => "https://github.com/boudra/zuck" },
+       files: ~w(lib README.md mix.exs LICENSE.md)
+     ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -29,6 +37,7 @@ defmodule Zuck.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:poison, "~> 3.0"},
       {:hackney, "~> 1.8.0"}
     ]
